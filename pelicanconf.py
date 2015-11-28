9ba3df620e10d0fb8c612d99172b8cf8a6392133
Jared Young <g42-github.com@gits.org>
2015-11-27 21:36:16 -0500

    updated some settings to remove comments and direct to my content

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,11 +23,10 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Ansible', 'http://www.ansible.com/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('GeekFortyTwo', 'https://twitter.com/geekfortytwo'),)
 
 DEFAULT_PAGINATION = False
 
