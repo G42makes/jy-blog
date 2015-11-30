fa5bf926e60bf42aec3b390f794a89eaf23cd5b0
Jared  Young <jyoung@2keys.ca>
2015-11-30 09:51:12 -0500

    bootstrap: added some comments related to migrating from older data, changed script to executable

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,8 @@
 #ToDo: make this script work on either RH or Debian based sysems as I use both
 
 #First order of business is to use pip to install 'pelican' and 'markdown'
-pip install pelican markdown
+sudo pip install pelican markdown
+
+#Notign here, but commented out, for import capability:
+#sudo pip install BeautifulSoup4 lxml
+#sudo yum install -y pandoc
