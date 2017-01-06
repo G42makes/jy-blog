83ebf6ea545fac81da2c0141b871814882cbea92
Jared Young <geek42-github.com@gits.org>
2017-01-06 17:38:58 -0500

    Bringing back some of the old pelican files for reference.

---
+++ blog/pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jared Young'
+SITENAME = u'Jared Young'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Ansible', 'http://www.ansible.com/'),)
+
+# Social widget
+SOCIAL = (('GeekFortyTwo', 'https://twitter.com/geekfortytwo'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
